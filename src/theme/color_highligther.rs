6dcdcdbbd155fd53eae7495f62202e637d1e1fc4
GZyannick <gzyannick@PC-Defaultstring>
2024-12-08 00:36:40 +0100

    impl color highlighting , not alot of time to code

--- src/theme/color_highligther.rs
+++ src/theme/color_highligther.rs
@@ -2,6 +2,11 @@ use crossterm::style::Color;
 
 use crate::log_message;
 
+use super::colors::{
+    BRIGHT_GREEN, BRIGHT_ORANGE, BRIGHT_RED, BRIGHT_WHITE, BRIGHT_YELLOW, FADED_BLUE, FADED_GREEN,
+    FADED_ORANGE, FADED_PURPLE, FADED_RED, GRAY_245, NEUTRAL_BLUE, NEUTRAL_GREEN, NEUTRAL_ORANGE,
+};
+
 #[derive(Debug)]
 pub struct ColorHighligter {
     pub start: usize,
@@ -10,34 +15,41 @@ pub struct ColorHighligter {
 }
 
 impl ColorHighligter {
-    fn get_color_from_punctuation(punctuation: &str) -> Color {
-        match punctuation {
-            "punctuation.function" => Color::Red,
-            "function" => Color::Red,
-            "property" => Color::Cyan,
-            "attribute" => Color::DarkYellow,
-            "constant.builtin" => Color::DarkRed,
-            "type.builtin" => Color::DarkMagenta,
-            "variable.builtin" => Color::DarkCyan,
-            "variable.parameter" => Color::DarkGreen,
-            "comment" => Color::Grey,
-            "punctuation.bracket" => Color::DarkBlue,
-            "punctuation.delimiter" => Color::Yellow,
-            "function.method" => Color::Red,
-            "function.macro" => Color::Yellow,
-            "punctuation.macro" => Color::Yellow,
-            "punctuation.builtin" => Color::Red,
-            "keyword" => Color::Green,
-            "constructor" => Color::Blue,
-            "type" => Color::Yellow,
-            "operator" => Color::Yellow,
-            "label" => Color::Yellow,
-            "string" => Color::Green,
-            s => {
-                log_message!("{s}");
-                Color::White
-            }
+    fn get_color(colors: (usize, usize, usize)) -> Color {
+        Color::Rgb {
+            r: colors.0 as u8,
+            g: colors.1 as u8,
+            b: colors.2 as u8,
         }
+    }
+
+    fn get_color_from_punctuation(punctuation: &str) -> Color {
+        let color = match punctuation {
+            "punctuation.function" => NEUTRAL_GREEN,
+            "function" => NEUTRAL_GREEN,
+            "property" => BRIGHT_RED,
+            "attribute" => NEUTRAL_BLUE,
+            "constant.builtin" => BRIGHT_RED,
+            "type.builtin" => BRIGHT_YELLOW,
+            "variable.builtin" => BRIGHT_WHITE,
+            "variable.parameter" => BRIGHT_YELLOW,
+            "comment" => GRAY_245,
+            "punctuation.bracket" => FADED_ORANGE,
+            "punctuation.delimiter" => BRIGHT_ORANGE,
+            "function.method" => BRIGHT_RED,
+            "function.macro" => BRIGHT_GREEN,
+            "punctuation.macro" => FADED_GREEN,
+            "punctuation.builtin" => BRIGHT_GREEN,
+            "keyword" => FADED_BLUE,
+            "constructor" => FADED_RED,
+            "type" => BRIGHT_YELLOW,
+            "operator" => NEUTRAL_ORANGE,
+            "label" => FADED_PURPLE,
+            "string" => FADED_GREEN,
+            s => BRIGHT_WHITE,
+        };
+
+        ColorHighligter::get_color(color)
     }
 
     pub fn new_from_capture(start: usize, end: usize, punctuation: &str) -> Self {
--- src/theme/colors.rs
+++ src/theme/colors.rs
@@ -18,39 +18,40 @@ pub const FILE_STATUS_BG: Color = Color::Rgb {
     b: 69,
 };
 
-const DARK0_HARD: (usize, usize, usize) = (29, 32, 34);
-const DARK0: (usize, usize, usize) = (40, 40, 41);
-const DARK0_SOFT: (usize, usize, usize) = (50, 48, 48);
-const DARK1: (usize, usize, usize) = (60, 56, 55);
-const DARK2: (usize, usize, usize) = (80, 73, 610);
-const DARK3: (usize, usize, usize) = (102, 92, 85);
-const DARK4: (usize, usize, usize) = (124, 111, 101);
-const GRAY_245: (usize, usize, usize) = (146, 131, 116);
+pub const DARK0_HARD: (usize, usize, usize) = (29, 32, 34);
+pub const DARK0: (usize, usize, usize) = (40, 40, 41);
+pub const DARK0_SOFT: (usize, usize, usize) = (50, 48, 48);
+pub const DARK1: (usize, usize, usize) = (60, 56, 55);
+pub const DARK2: (usize, usize, usize) = (80, 73, 610);
+pub const DARK3: (usize, usize, usize) = (102, 92, 85);
+pub const DARK4: (usize, usize, usize) = (124, 111, 101);
+pub const GRAY_245: (usize, usize, usize) = (146, 131, 116);
 
-const BRIGHT_RED: (usize, usize, usize) = (251, 73, 52);
-const BRIGHT_GREEN: (usize, usize, usize) = (184, 187, 38);
-const BRIGHT_YELLOW: (usize, usize, usize) = (250, 189, 47);
-const BRIGHT_BLUE: (usize, usize, usize) = (131, 165, 152);
-const BRIGHT_PURPLE: (usize, usize, usize) = (211, 134, 155);
-const BRIGHT_AQUA: (usize, usize, usize) = (142, 192, 124);
-const BRIGHT_ORANGE: (usize, usize, usize) = (254, 128, 25);
+pub const BRIGHT_RED: (usize, usize, usize) = (251, 73, 52);
+pub const BRIGHT_GREEN: (usize, usize, usize) = (184, 187, 38);
+pub const BRIGHT_YELLOW: (usize, usize, usize) = (250, 189, 47);
+pub const BRIGHT_BLUE: (usize, usize, usize) = (131, 165, 152);
+pub const BRIGHT_PURPLE: (usize, usize, usize) = (211, 134, 155);
+pub const BRIGHT_AQUA: (usize, usize, usize) = (142, 192, 124);
+pub const BRIGHT_ORANGE: (usize, usize, usize) = (254, 128, 25);
 
-const NEUTRAL_RED: (usize, usize, usize) = (204, 36, 29);
-const NEUTRAL_GREEN: (usize, usize, usize) = (152, 151, 26);
-const NEUTRAL_YELLOW: (usize, usize, usize) = (215, 153, 33);
-const NEUTRAL_BLUE: (usize, usize, usize) = (69, 133, 136);
-const NEUTRAL_PURPLE: (usize, usize, usize) = (177, 98, 134);
-const NEUTRAL_AQUA: (usize, usize, usize) = (104, 157, 106);
-const NEUTRAL_ORANGE: (usize, usize, usize) = (214, 93, 14);
+pub const NEUTRAL_RED: (usize, usize, usize) = (204, 36, 29);
+pub const NEUTRAL_GREEN: (usize, usize, usize) = (152, 151, 26);
+pub const NEUTRAL_YELLOW: (usize, usize, usize) = (215, 153, 33);
+pub const NEUTRAL_BLUE: (usize, usize, usize) = (69, 133, 136);
+pub const NEUTRAL_PURPLE: (usize, usize, usize) = (177, 98, 134);
+pub const NEUTRAL_AQUA: (usize, usize, usize) = (104, 157, 106);
+pub const NEUTRAL_ORANGE: (usize, usize, usize) = (214, 93, 14);
 
-const FADED_RED: (usize, usize, usize) = (157, 0, 6);
-const FADED_GREEN: (usize, usize, usize) = (121, 116, 14);
-const FADED_YELLOW: (usize, usize, usize) = (181, 118, 20);
-const FADED_BLUE: (usize, usize, usize) = (7, 102, 120);
-const FADED_PURPLE: (usize, usize, usize) = (143, 63, 113);
-const FADED_AQUA: (usize, usize, usize) = (66, 123, 88);
-const FADED_ORANGE: (usize, usize, usize) = (175, 58, 3);
+pub const FADED_RED: (usize, usize, usize) = (157, 0, 6);
+pub const FADED_GREEN: (usize, usize, usize) = (121, 116, 14);
+pub const FADED_YELLOW: (usize, usize, usize) = (181, 118, 20);
+pub const FADED_BLUE: (usize, usize, usize) = (7, 102, 120);
+pub const FADED_PURPLE: (usize, usize, usize) = (143, 63, 113);
+pub const FADED_AQUA: (usize, usize, usize) = (66, 123, 88);
+pub const FADED_ORANGE: (usize, usize, usize) = (175, 58, 3);
 
+pub const BRIGHT_WHITE: (usize, usize, usize) = (255, 255, 255);
 // const LIGHT0_HARD: (usize, usize, usize) = (249, 245, 215);
 // const LIGHT0: (usize, usize, usize) = (253, 244, 193);
 // const LIGHT0_SOFT: (usize, usize, usize) = (242, 229, 188);
