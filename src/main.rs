09683addba15683e3f2970346f5a0ee1635e8f83
yannick Gouez <yannickgouez@MacBook-Pro-de-yannick.local>
2025-02-12 16:29:32 +0100

    main.rs basic test

--- src/main.rs
+++ src/main.rs
@@ -33,3 +33,53 @@ fn main() -> anyhow::Result<()> {
 
     Ok(())
 }
+
+#[cfg(test)]
+mod main_tests {
+    use crate::{
+        buff::Buffer,
+        editor::{core::mode::Mode, Editor},
+    };
+
+    #[test]
+    fn check_file_path_buffer() {
+        let path = "./src/main.rs".to_string();
+        let file_path = Some(path.clone());
+        let buffer = Buffer::new(file_path);
+        assert!(!buffer.is_directory);
+        assert_eq!(buffer.path, path);
+    }
+    #[test]
+    fn check_folder_path_buffer() {
+        let file_path = Some("./".to_string());
+        let buffer = Buffer::new(file_path);
+        assert!(buffer.is_directory);
+        assert_eq!(buffer.path, "./".to_string());
+    }
+
+    #[test]
+    fn check_empty_path_buffer() {
+        let file_path = None;
+        let buffer = Buffer::new(file_path);
+        assert!(!buffer.is_directory);
+        assert_eq!(buffer.path, "Empty".to_string());
+    }
+
+    #[test]
+    fn check_editor() -> anyhow::Result<()> {
+        let path = "./src/main.rs".to_string();
+        let file_path = Some(path.clone());
+        let buffer = Buffer::new(file_path);
+
+        let editor = Editor::new(buffer)?;
+        assert!(matches!(editor.mode, Mode::Normal));
+        assert!(!editor.keybinds.normal_mode.is_empty());
+        assert!(!editor.keybinds.visual_mode.is_empty());
+        assert!(!editor.keybinds.insert_mode.is_empty());
+        assert!(!editor.keybinds.command_mode.is_empty());
+        assert!(!editor.keybinds.file_explorer.is_empty());
+        assert!(!editor.keybinds.search_mode.is_empty());
+        assert!(editor.viewports.values.len() == 2);
+        Ok(())
+    }
+}
