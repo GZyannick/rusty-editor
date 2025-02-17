948631e05d726cb643918af1b0122e2096a61afe
yannick Gouez <yannickgouez@MacBook-Pro-de-yannick.local>
2025-02-17 12:59:11 +0100

    search test

--- src/editor/core/actions/search.rs
+++ src/editor/core/actions/search.rs
@@ -47,3 +47,112 @@ impl Action {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests_search {
+    use tempfile::{tempfile, NamedTempFile};
+
+    use crate::{
+        buff::Buffer,
+        editor::{
+            core::{actions::action::Action, mode::Mode},
+            Editor,
+        },
+        log_message,
+    };
+    use std::{
+        fs::File,
+        io::{Cursor, Seek, Write},
+    };
+
+    fn setup_temp_file() -> NamedTempFile {
+        let mut temp_file = NamedTempFile::new().expect("Failed to create a temporary_file");
+        let content =
+            "This is a test file with multiple line.\nHere is a keyword we will search.\nAnother line with keyword.";
+        temp_file
+            .write_all(content.as_bytes())
+            .expect("Failed to write to temp file");
+        temp_file.flush().expect("Failed to flush temp file");
+
+        // return the cursor at 0:0
+        temp_file
+            .seek(std::io::SeekFrom::Start(0))
+            .expect("Failed to seek temp file");
+        temp_file
+    }
+
+    fn create_mock_editor() -> Editor<Cursor<Vec<u8>>> {
+        Editor::default()
+    }
+    #[test]
+    fn test_clear_to_normal_mode() {
+        let mut editor = create_mock_editor();
+        editor.mode = Mode::Search;
+        editor.search = "Some search".to_string();
+        Action::ClearToNormalMode.execute(&mut editor).unwrap();
+        assert!(editor.search.is_empty(), "Search string should be Empty");
+        assert!(
+            matches!(editor.mode, Mode::Normal),
+            "Search string should be Empty"
+        );
+    }
+
+    #[test]
+    fn test_find_search_value() {
+        let tmp_file = setup_temp_file();
+        let path = tmp_file.path().to_str().unwrap().to_string();
+        let mut editor = create_mock_editor();
+        let old_cursor = editor.cursor;
+        editor.viewports.c_mut_viewport().buffer = Buffer::new(Some(path));
+
+        editor.search = "line".to_string();
+        editor.mode = Mode::Search;
+
+        Action::FindSearchValue.execute(&mut editor).unwrap();
+        assert!(
+            !editor.viewports.c_mut_viewport().search_pos.is_empty(),
+            "search_pos should not be empty"
+        );
+        assert!(
+            editor.viewports.c_mut_viewport().search_pos.len() == 2,
+            "search_pos should have 2 occurences"
+        );
+
+        assert!(
+            editor.cursor != old_cursor,
+            "search_pos should have 2 occurences"
+        );
+    }
+
+    #[test]
+    fn test_iter_next_search() {
+        let tmp_file = setup_temp_file();
+        let path = tmp_file.path().to_str().unwrap().to_string();
+        let mut editor = create_mock_editor();
+        editor.viewports.c_mut_viewport().buffer = Buffer::new(Some(path));
+
+        editor.search = "line".to_string();
+        editor.mode = Mode::Search;
+
+        Action::FindSearchValue.execute(&mut editor).unwrap();
+        assert!(
+            !editor.viewports.c_mut_viewport().search_pos.is_empty(),
+            "search_pos should not be empty"
+        );
+        assert!(
+            editor.viewports.c_mut_viewport().search_index == 0,
+            "search_index should be 0"
+        );
+
+        let old_cursor = editor.cursor;
+        Action::IterNextSearch.execute(&mut editor).unwrap();
+        assert!(
+            editor.viewports.c_mut_viewport().search_index == 1,
+            "search_index should be 1"
+        );
+        assert!(
+            editor.cursor != old_cursor,
+            "cursor should be different after iterating"
+        );
+    }
+}
