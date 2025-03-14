2eb615004634ee7006036800137f478fa1013436
yannick Gouez <yannickgouez@MacBook-Pro-de-yannick.local>
2025-03-14 13:57:01 +0100

    create default_theme

--- src/theme/colors.rs
+++ src/theme/colors.rs
@@ -1,13 +1,7 @@
 pub const _DARK0_HARD: (u8, u8, u8) = (29, 32, 34);
-// real pub const DARK0: (u8, u8, u8) = (40, 40, 41);
-pub const DARK0: (u8, u8, u8) = (0, 0, 0); // this is temporary to have a different bg between
-                                           // my editor and vim btw
+pub const DARK0: (u8, u8, u8) = (40, 40, 41);
 pub const DARK0_SOFT: (u8, u8, u8) = (50, 48, 48);
-// real pub const DARK1: (u8, u8, u8) = (60, 56, 55);
-pub const DARK1: (u8, u8, u8) = (40, 40, 40);
-pub const _DARK2: (u8, u8, u8) = (80, 73, 61);
-pub const _DARK3: (u8, u8, u8) = (102, 92, 85);
-pub const _DARK4: (u8, u8, u8) = (124, 111, 101);
+pub const DARK1: (u8, u8, u8) = (60, 56, 55);
 pub const GRAY_245: (u8, u8, u8) = (146, 131, 116);
 
 pub const BRIGHT_RED: (u8, u8, u8) = (251, 73, 52);
@@ -21,19 +15,8 @@ pub const BRIGHT_ORANGE: (u8, u8, u8) = (254, 128, 25);
 pub const NEUTRAL_RED: (u8, u8, u8) = (204, 36, 29);
 pub const NEUTRAL_GREEN: (u8, u8, u8) = (152, 151, 26);
 pub const NEUTRAL_YELLOW: (u8, u8, u8) = (215, 153, 33);
-pub const _NEUTRAL_BLUE: (u8, u8, u8) = (69, 133, 136);
-pub const _NEUTRAL_PURPLE: (u8, u8, u8) = (177, 98, 134);
 pub const NEUTRAL_AQUA: (u8, u8, u8) = (104, 157, 106);
-pub const _NEUTRAL_ORANGE: (u8, u8, u8) = (214, 93, 14);
 
-pub const _FADED_RED: (u8, u8, u8) = (157, 0, 6);
-pub const _FADED_GREEN: (u8, u8, u8) = (121, 116, 14);
-pub const _FADED_YELLOW: (u8, u8, u8) = (181, 118, 20);
-pub const _FADED_BLUE: (u8, u8, u8) = (7, 102, 120);
 pub const FADED_PURPLE: (u8, u8, u8) = (143, 63, 113);
-pub const _FADED_AQUA: (u8, u8, u8) = (66, 123, 88);
-pub const _FADED_ORANGE: (u8, u8, u8) = (175, 58, 3);
-
 pub const BRIGHT_WHITE: (u8, u8, u8) = (255, 255, 255);
-
 pub const LIGTH_GREY: (u8, u8, u8) = (91, 91, 93);
